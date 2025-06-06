749c09b279ea7cc935aadf74866d099af854ba84
hidekazu21 <hidekazubee@gmail.com>
2025-06-06 10:50:59 +0900

    DB接続変更

--- check.php
+++ check.php
@@ -1,5 +1,5 @@
 <?php
-    $pdo=new PDO('mysql:host=mysql304.phy.lolipop.lan;dbname=LAA1554872-php2024;charset=utf8','LAA1554872','Pass1005');
+    $pdo=new PDO('mysql:host=mysql320.phy.lolipop.lan;dbname=LAA1554902-shortbbs;charset=utf8','LAA1554902','Short1234');
     $sql=$pdo->prepare('select * from user where mail=? and password=?');
     $sql->execute([$_POST['mail'],$_POST['pw']]);
     foreach($sql as $row){
