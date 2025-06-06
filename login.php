39a153abeeaa651b3e3265349f42b6fda2f418e7
Yanagi5420 <take5oct@gmail.com>
2025-06-06 10:04:18 +0900

    login

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>一言掲示板 - ログイン</title>
+</head>
+<body>
+    <h1>ログイン</h1>
+    <form action="login.php" method="post">
+        <p>ユーザー名：<input type="text" name="name" required></p>
+        <p>パスワード：<input type="password" name="name" required></p>
+        <p><button type="submit">ログイン</button></p>
+    </form>
+</body>
+</html>
