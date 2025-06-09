3b37b577ae15915132194feed598bbeb1a989286
hidekazu21 <hidekazubee@gmail.com>
2025-06-09 10:50:56 +0900

    DBに接続・出力されるように変更

--- view.php
+++ view.php
@@ -6,18 +6,35 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+
+    <?php
+        // データベースに接続
+        try {
+            $pdo=new PDO('mysql:host=mysql320.phy.lolipop.lan;dbname=LAA1554902-shortbbs;charset=utf8','LAA1554902','Short1234');
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo '接続失敗: ' . $e->getMessage();
+        }
+
+        //全件取得
+        $stmt = $pdo->query("SELECT user_id,content,create_at FROM comment");
+        $comment = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    ?>
+
     <h1>📜 投稿一覧</h1>
     <p><a href="form.php">← 投稿フォームへ戻る</a></p>
     <hr>
     <?php
-    $filename = 'comments.txt';
-    if (file_exists($filename)) {
-        $lines = file($filename, FILE_IGNORE_NEW_LINES);
-        foreach (array_reverse($lines) as $line) {
-            [$time, $name, $comment] = explode("\t", $line);
+    if (!empty($comment)) {
+        foreach ($comment as $comments) {
             echo "<div class='post'>";
-            echo "<p><strong>$name</strong> さん ($time)</p>";
-            echo "<p>" . nl2br($comment) . "</p>";
+            //user_idから名前を呼び出し
+            $stmt = $pdo->query("SELECT user_name FROM users WHERE id = $comments[user_id]");
+            $name = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            echo "<p><strong>$name[user_name]</strong>さん
+            (".htmlspecialchars($comments['create_at']).")</p>";
+            echo "<p>" . nl2br(htmlspecialchars($comments['content'])) . "</p>";
             echo "</div><hr>";
         }
     } else {
