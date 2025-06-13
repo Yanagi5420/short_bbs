33087554e855418b0a49c4d52a7edad103a1eb1a
Yanagi5420 <take5oct@gmail.com>
2025-06-13 10:38:50 +0900

    form session変更

--- form.php
+++ form.php
@@ -1,4 +1,10 @@
-<?php session_start(); ?>
+<?php
+session_start();
+if (!isset($_SESSION['name'])) {
+    header("Location: login.php");
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
